2a4fc3b49069c3462b602a5313115ea17f4beceb
Jagadeesan20 <p.jagadish07112002@gmail.com>
2021-09-02 18:12:43 +0530

    all change
    Signed-off-by: Jagadeesan20 <p.jagadish07112002@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React,{useEffect,useState} from 'react'
 import axios from 'axios'
 const App = () => {
-  const[post,setPost]=useState([])
+  const[post,setPost]=useState({})
   const[show,setShow]=useState(false)
+  const[id,setId]=useState(1)
+  const[idFrom,setIdFrom]=useState(1)
+
+  const handleClick=()=>{
+    setIdFrom(id)
+  }
   
   useEffect(()=>{
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idFrom}`)
       .then(res=>{
         console.log(res)
         setPost(res.data)
@@ -13,13 +19,15 @@ const App = () => {
       .catch((err)=>{
         console.log(err)
       })
-  },[])
+  },[idFrom])
   return (
     <div>
       <h1>Data fetching using React</h1>
+      <input type='text' value={id} onChange={e=>setId(e.target.value)}></input>
+      <button type='button' onClick={handleClick}>Fetch Post</button>
       <button onClick={()=>setShow(!show)}>Toggle</button>
-      {show &&
-      <p>{post.map(posts=><li key={posts.id}>{posts.title}</li>)}</p>}
+      {show && <div>{post.title}</div>}
+      
     </div>
   )
 }
